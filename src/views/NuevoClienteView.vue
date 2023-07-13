d1361707ee5b1b5c9e2406bea565528f8af23e98
mvazquezma <mvm51090@gmail.com>
2023-07-13 17:09:03 +0200

    Added formkit plugin and form structure done

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,4 +1,5 @@
 <script setup>
+    import { FormKit } from '@formkit/vue'
     import Heading from '../components/UI/Heading.vue';
     import RouterLink from '../components/UI/RouterLink.vue';
 
@@ -18,5 +19,66 @@
         </div>
 
         <Heading>{{ titulo }}</Heading>
+
+        <div class="mx-auto mt-10 bg-white shadow">
+            <div class="mx-auto md:w-2/3 py-20 px-6">
+                 <FormKit
+                    type="form"
+                    submit-label="Agregar Cliente"
+                    incomplete-message="No se pudo enviar, revisa los mensajes"
+                >
+                    <FormKit 
+                        type="text"
+                        label="Nombre"
+                        placeholder="Nombre de cliente"
+                        validation="required"
+                        :validation-messages="{ required: 'El nombre del cliente es obligatorio' }"
+                    />
+
+                    <FormKit 
+                        type="text"
+                        label="Apellido"
+                        placeholder="Apellido de cliente"
+                        validation="required"
+                        :validation-messages="{ required: 'El apellido del cliente es obligatorio' }"
+                    />
+
+                    <FormKit 
+                        type="email"
+                        label="Email"
+                        placeholder="Email de cliente"
+                        validation="required|email"
+                        :validation-messages="{ required: 'El email del cliente es obligatorio', email: 'Coloca un email válido' }"
+                    />
+
+                    <FormKit
+                        type="text"
+                        label="Teléfono"
+                        placeholder="Teléfono: XXX-XXX-XXXX"
+                        validation="?matches:/[^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+                        :validation-messages="{ matches: 'El formato no es válido' }"
+                    />
+
+                    <FormKit 
+                        type="text"
+                        label="Empresa"
+                        placeholder="Empresa de cliente"
+                    />
+
+                    <FormKit 
+                        type="text"
+                        label="Puesto"
+                        placeholder="Puesto de cliente"
+                    />
+                </FormKit>
+            </div>
+           
+        </div>
     </div>
 </template>
+
+<style>
+.formkit-wrapper {
+    max-width: 100%;
+}
+</style>
